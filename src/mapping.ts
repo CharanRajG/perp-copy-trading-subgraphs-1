850b768a3978e9cff5954bac0ce1b4d42721c674
Nuthan Prasad <nuthan1159@gmail.com>
2023-04-12 21:18:34 +0530

    mappings, schema, yaml defined

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,54 +1,21 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import {
-  VaultFactory,
-  CreateVault,
-  DeleteVault
-} from "../generated/VaultFactory/VaultFactory"
-import { ExampleEntity } from "../generated/schema"
+import { Address, BigInt } from "@graphprotocol/graph-ts";
+import { CreateVault, DeleteVault } from "../generated/VaultFactory/VaultFactory";
+import { Vault } from "../generated/schema";
 
 export function handleCreateVault(event: CreateVault): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.creator = event.params.creator
-  entity.name = event.params.name
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.MANAGEMENT_FEE(...)
-  // - contract.governor(...)
-  // - contract.keeper(...)
-  // - contract.vaults(...)
+  let vault = new Vault(event.params.vaultProxy.toHex());
+  vault.creator = event.params.creator;
+  vault.name = event.params.name.toString();
+  vault.implementation = event.params.vaultImplementation;
+  vault.proxy = event.params.vaultProxy;
+  vault.status = "open";
+  vault.save();
 }
 
-export function handleDeleteVault(event: DeleteVault): void {}
+export function handleDeleteVault(event: DeleteVault): void {
+  let vault = Vault.load(event.params.vaultProxy.toHex());
+  if (vault != null) {
+    vault.status = "closed";
+    vault.save();
+  }
+}
